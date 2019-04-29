1162ce19a0f9cf6fcbd310fcece9bbf5933cd17f
dzj0821 <dzj0821@126.com>
2019-04-29 20:42:18 +0800

    delete useless import delete repeat dependency

--- src/main/java/pers/dzj0821/SchoolLeaveSystem/controller/IndexController.java
+++ src/main/java/pers/dzj0821/SchoolLeaveSystem/controller/IndexController.java
@@ -1,7 +1,6 @@
 package pers.dzj0821.SchoolLeaveSystem.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
